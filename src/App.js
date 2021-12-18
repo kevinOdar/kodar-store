fa6f14f03cbf6e37c39557a833c10e51e383256f
Kevin Odar <kevodar@gmail.com>
2021-12-18 02:15:11 -0300

    updating for router v6

--- src/App.js
+++ src/App.js
@@ -5,26 +5,22 @@ import Sidebar from './components/Sidebar';
 import ProductsPage from './pages/ProductsPage';
 import CartPage from './pages/CartPage';
 
-const App = () => {
-  return (
-    <Router>
-      <Header />
-      <Sidebar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
+const App = () => <Router>
+  <Header />
+  <Sidebar />
+  <Routes>
+    <Route exact path="/" element={<Home />} />
 
-        <Route exact path="/about" element={<About />} />
+    <Route exact path="/about" element={<About />} />
 
-        <Route exact path="/products" element={<ProductsPage />} />
+    <Route exact path="/products" element={<ProductsPage />} />
 
-        <Route exact path="/cart" element={<CartPage />} />
+    <Route exact path="/cart" element={<CartPage />} />
 
-        <Route exact path="/products/:id" element={<SingleProduct />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
-};
+    <Route exact path="/products/:id" element={<SingleProduct />} />
+    <Route path="*" element={<Error />} />
+  </Routes>
+  <Footer />
+</Router>
 
 export default App;
